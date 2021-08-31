2ead5012e31fb93f15fc433a537fc7f94708ffce
Kimberly Arcena <kimanecra143@gmail.com>
2021-08-31 23:44:46 +0800

    initialization of openCV & camera

--- app/src/main/java/com/example/facerecognition/MainActivity.java
+++ app/src/main/java/com/example/facerecognition/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.facerecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -17,9 +20,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private Button camera_button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera_button.findViewById(R.id.cameraBtn);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 }
